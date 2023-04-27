61f8300091c97bbb55e8ef9f19311828349c891c
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-04-27 11:39:31 -0600

    connect to onyx sql

--- ClassManagement.java
+++ ClassManagement.java
@@ -1,43 +1,55 @@
+import java.sql.*;
 import java.util.Scanner;
 
 public class ClassManagement {
     
     public static void main(String[] args) {
+        try {
+            Connection con = Database.getDatabaseConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        boolean running = true;
+
         System.out.println("Class Management");
         System.out.println("Category and Assignment Management");
         System.out.println("Student Management");
-        System.out.println("Grade Reportin");
+        System.out.println("Grade Reporting");
         System.out.println("Grade Calculation");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        input = input.toLowerCase();
+        String input = "";
+        
+        while (running){
+            input = scanner.next();
+            input = input.toLowerCase();
 
-        switch(input){
-            case "class management":
-                    System.out.print("Create a class: ");
-                    Scanner sc = new Scanner(scanner.next());
-                    sc.useDelimiter(" ");
-                    String courseNumber = sc.next();
-                    String term = "";
-                    int sectionNumber = 0;
-                    if(sc.hasNext()){
-                        term = sc.next();
+            switch(input){
+                case "class management":
+                        System.out.print("Create a class: ");
+                        Scanner sc = new Scanner(scanner.next());
+                        sc.useDelimiter(" ");
+                        String courseNumber = sc.next();
+                        String term = "";
+                        int sectionNumber = 0;
                         if(sc.hasNext()){
-                            sectionNumber = sc.nextInt();
+                            term = sc.next();
                             if(sc.hasNext()){
                                 sectionNumber = sc.nextInt();
+                                if(sc.hasNext()){
+                                    sectionNumber = sc.nextInt();
+                                }
                             }
+
                         }
 
-                    }
 
-
-                    // reateClass()
-                break;
-            default:
-                System.out.println("invalid selection");
-                break;
+                        // reateClass()
+                    break;
+                default:
+                    System.out.println("invalid selection");
+                    break;
+            }
         }
     }
 }
--- Database.java
+++ Database.java
@@ -2,22 +2,18 @@ import java.sql.*;
 
 public class Database {
     public static Connection getDatabaseConnection() throws SQLException {
-        int databasePort = 0000;//???
-        String databaseHost = "";
-        String databaseUsername = "";
-        String databasePassword = "";
-        String databaseName = "";
-        String databaseURL = String.format(
-                "jdbc:mysql://%s:%s/%s?verifyServerCertificate=false&useSSL=false&serverTimezone=UTC",
-                databaseHost,
-                databasePort,
-                databaseName);
-
+        int databasePort = 52705;//???
+        String databasePassword = "sql";
         try {
-
-            return DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+            System.out.println("jdbc:mysql://localhost:"+databasePort+"/test?verifyServerCertificate=false&useSSL=true");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:"+databasePort+"/test?verifyServerCertificate=false&useSSL=true&serverTimezone=UTC", "msandbox",
+                    databasePassword);
+            // Do something with the Connection
+            System.out.println("Database [test db] connection succeeded!");
+            System.out.println();
+            return con;
         } catch (SQLException sqlException) {
-            System.out.printf("SQLException was thrown while trying to connection to database: %s%n", databaseURL);
+            System.out.println("SQLException was thrown while trying to connection to database: \njdbc:mysql://localhost:"+databasePort+"/test?verifyServerCertificate=false&useSSL=true");
             System.out.println(sqlException.getMessage());
             throw sqlException;
         }
